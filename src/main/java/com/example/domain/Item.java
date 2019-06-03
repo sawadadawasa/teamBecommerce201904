83fc3769dd02f9be925ee892aebb33f17e083c8f
shogo.sawada <shogo.sawada@rakus.co.jp>
2019-06-03 10:32:32 +0900

    OrderItemとItemのドメインを作成

---
+++ src/main/java/com/example/domain/Item.java
@@ -0,0 +1,56 @@
+package com.example.domain;
+
+public class Item {
+	
+	private Integer id; //商品ID
+	private String name; //商品名
+	private String desicription; //説明
+	private String imagePath; //画像データ
+	private boolean deleted; //削除フラグ
+	private int piece; //パック数
+	private String origin; //原産地
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDesicription() {
+		return desicription;
+	}
+	public void setDesicription(String desicription) {
+		this.desicription = desicription;
+	}
+	public String getImagePath() {
+		return imagePath;
+	}
+	public void setImagePath(String imagePath) {
+		this.imagePath = imagePath;
+	}
+	public boolean isDeleted() {
+		return deleted;
+	}
+	public void setDeleted(boolean deleted) {
+		this.deleted = deleted;
+	}
+	public int getPiece() {
+		return piece;
+	}
+	public void setPiece(int piece) {
+		this.piece = piece;
+	}
+	public String getOrigin() {
+		return origin;
+	}
+	public void setOrigin(String origin) {
+		this.origin = origin;
+	}
+
+}
