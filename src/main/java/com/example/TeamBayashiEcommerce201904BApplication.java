4c1298f02b1e2f1fbb80ca842cf529abcbee0f54
shogo.sawada <shogo.sawada@rakus.co.jp>
2019-06-03 09:28:17 +0900

    first commit

---
+++ src/main/java/com/example/TeamBayashiEcommerce201904BApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamBayashiEcommerce201904BApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamBayashiEcommerce201904BApplication.class, args);
+	}
+
+}
