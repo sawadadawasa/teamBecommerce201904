88e709a1f6c4b9c0f8e511442ecbe9cfe3f54e53
kambayashikaede <kaede.kambayashi@rakus.co.jp>
2019-06-03 17:39:42 +0900

    新規登録画面を表示（暫定）

--- src/main/java/com/example/web/UserController.java
+++ src/main/java/com/example/web/UserController.java
@@ -2,9 +2,11 @@ package com.example.web;
 
 import java.lang.ProcessBuilder.Redirect;
 import java.lang.reflect.Member;
+import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -12,6 +14,7 @@ import org.springframework.validation.ObjectError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.domain.User;
 import com.example.service.UserService;
@@ -42,6 +45,7 @@ public class UserController {
 	@RequestMapping(value="/login")
 	public String login(@Validated UserForm form,
 			BindingResult result) {
+		System.out.println("mmmmmm");
 		if(result.hasErrors()) {
 			return index();
 		}
@@ -49,16 +53,50 @@ public class UserController {
 		String email=form.getEmail();
 		String password=form.getPassword();
 		User user=userService.findOneByEmailAndPassword(email, password);
-		if(user == null) {
-			ObjectError error = new ObjectError("loginerror", "メールアドレスまたはパスワードが違います。");
-            result.addError(error);
-			return index();
-		}
+		System.out.println("kkkkkk");
+		
 		session.setAttribute("user", user);
 		
-		System.out.println("mmmmmm");
+		System.out.println("nnnnnn");
 		return "redirect:/js/login";
 		
+	}
+	
+	//メンバー登録画面
+	@RequestMapping(value = "/form")
+	public String form() {
+		System.out.println("aaa");
+		return "newAccount";
+	}
+	
+	//メンバー情報を登録します。
+	@RequestMapping(value = "create")
+	public String create(@Validated UserForm form, 
+			BindingResult result,RedirectAttributes redirectAttributes) {
+		if(result.hasErrors()) {
+			return form();
+		}
+		List<User> users = userService.findAll();
+		for (User user : users) {
+			if ((user.getEmail()).equals(form.getEmail())) {
+				ObjectError error = new ObjectError("addresserror", "このメールアドレスは既に登録されています。");
+				result.addError(error);
+				return form();
+			}
+		}
+		
+		//確認用パスワードとパスワードが一致しなければエラー
+		String password=form.getPassword();
+		String passwordConfirm=form.getPasswordConfirm();
+		if(!passwordConfirm.equals(password)) {
+			ObjectError error=new ObjectError("loginerror","パスワードと確認用パスワードが一致しません。" );
+			 result.addError(error);
+			 return form();
+		}
+		User user = new User();
+		BeanUtils.copyProperties(form, user);
+		userService.save(user);
+		return "redirect:/";
 	}
 	
 
