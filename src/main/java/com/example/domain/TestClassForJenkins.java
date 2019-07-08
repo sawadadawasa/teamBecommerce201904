8b59fc3ecf481a2ebb926f9e6199c6c60f13ce48
shogo.sawada <shogo.sawada@rakus-partners.co.jp>
2019-07-08 16:09:09 +0900

    テストその２！テストクラス作りましたよ

---
+++ src/main/java/com/example/domain/TestClassForJenkins.java
@@ -0,0 +1,21 @@
+package com.example.domain;
+
+public class TestClassForJenkins {
+	
+	private String jenkins;
+	private int jenkinsNum;
+	
+	public String getJenkins() {
+		return jenkins;
+	}
+	public void setJenkins(String jenkins) {
+		this.jenkins = jenkins;
+	}
+	public int getJenkinsNum() {
+		return jenkinsNum;
+	}
+	public void setJenkinsNum(int jenkinsNum) {
+		this.jenkinsNum = jenkinsNum;
+	}
+
+}
